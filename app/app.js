008233c6a548848a40a6f915d288d4226efc11de
Samuel Ward <148538355+SamuelTWard@users.noreply.github.com>
2025-05-27 18:43:30 +0100

    initial map setup

---
+++ app/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+{
+	console.log('Server running on http://localhost:' + PORT);
+});
+
+app.get('/', (req, res) =>
+{
+	res.render('map.ejs', { API_URL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDx1nDqigjyOixfMY4kj485EaIkEi1VXX0&callback=initMap" });
+});
