d8d9a9fc48b2c7063e6e09535a4aa24b7349855d
giorgioellis <giorgioellis@hotmail.com>
2025-05-30 15:48:31 +0100

    Sorted page not found handling

--- app/app.js
+++ app/app.js
@@ -5,14 +5,18 @@ const PORT = 3000
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.listen(PORT, () =>
-{
-	console.log('Server running on http://localhost:' + PORT);
-});
-
 app.get('/', (req, res) =>
 {
 	res.render('layout.ejs', { title: "Map", mainContent: "pages/map", includeParams: { API_URL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDx1nDqigjyOixfMY4kj485EaIkEi1VXX0&loading=async&callback=initMap" } });
 });
 
-//todo page not found
+// Handler for when page is not found
+app.use((req, res) => {
+		res.status(404).send("Page Not Found");
+	}
+);
+
+app.listen(PORT, () =>
+{
+	console.log('Server running on http://localhost:' + PORT);
+});
