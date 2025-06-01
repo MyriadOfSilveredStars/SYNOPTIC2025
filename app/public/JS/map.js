6061b5e21bb51070c10e7d8092842d9547384630
giorgioellis <giorgioellis@hotmail.com>
2025-06-01 17:42:36 +0100

    Remove fix as didn't work

--- app/public/JS/map.js
+++ app/public/JS/map.js
@@ -208,5 +208,3 @@ async function initMap() //called by google maps API once loaded
 		console.log("Marker placed successfully!");
 	}
 }
-
-window.initMap = initMap; // Make this function global so that Google Maps API can run it once loaded
