42195e816837248e311475f2f79bb0b32157dc21
upj23pfu <148789541+upj23pfu@users.noreply.github.com>
2025-06-06 17:50:48 +0100

    added resetting password and verifying user

---
+++ models/markerModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const general = require('../controllers/generalController');
+const security = require('../controllers/securityController');
+const crypto = require('crypto');
+
+const markerSchema = new mongoose.Schema({
+    id: { type: String, default: () => crypto.randomUUID() },
+    position: {
+        lat: Number,
+        lng: Number,
+    },
+    creator: String,
+    upvotes: {type: Number, default: 0},
+    downvotes: {type: Number, default: 0},
+    description: String,
+    markerDate: Date,
+    markerType: String,
+    upVoterList: { type: Array, default: [] },
+    downVoterList: { type: Array, default: [] }
+});
+
+const Marker = mongoose.model('Marker', markerSchema);
+
+
+module.exports = Marker;
