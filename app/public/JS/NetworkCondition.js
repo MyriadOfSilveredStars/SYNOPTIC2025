513eaa773544958975effaebcf4caff2e16d1ff9
Samuel Ward <esj23xpu@uea.ac.uk>
2025-06-07 20:15:15 +0100

    network connection thingy

--- app/public/JS/NetworkCondition.js
+++ app/public/JS/NetworkCondition.js
@@ -1,3 +1,5 @@
+var countdownMessageDismiss = 0;
+
 window.onload = () => {
     function TestConnection() {
         //inspiration from https://www.codingnepalweb.com/detect-internet-connection-javascript/
@@ -20,8 +22,30 @@ window.onload = () => {
     }, 500);
 };
 
+
 function ConnectionStatusSet(isConnected) {
-    document.getElementById("network-condition").style.display = isConnected
-        ? "none"
-        : "flex";
+    div = document.getElementById("network-condition");
+    
+    if (isConnected) {
+        if (countdownMessageDismiss <= 0) {
+            div.style.display = "none";
+        }
+        else
+        {
+            countdownMessageDismiss--;
+            if (countdownMessageDismiss < 8)
+            {
+                div.style.display = "flex";
+                div.innerHTML = `<i class="fa-solid fa-check"></i> &nbsp Back Online`;
+                div.style.backgroundColor = "rgb(200, 252, 200)";
+            }
+        }
+    }
+    else
+    {
+        countdownMessageDismiss = 10;
+        div.style.display = "flex";
+        div.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> &nbsp No Connection`;
+        div.style.backgroundColor = "rgb(252, 203, 200)";
+    }
 }
